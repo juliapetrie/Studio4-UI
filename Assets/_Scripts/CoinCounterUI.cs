f774c93cd90c9d365954822373ae5bef253341cc
juliapetrie <julia.petrie@icloud.com>
2025-03-06 10:02:54 -0800

    [C] updated coin counter

--- Assets/_Scripts/CoinCounterUI.cs
+++ Assets/_Scripts/CoinCounterUI.cs
@@ -16,6 +16,7 @@ public class CoinCounterUI : MonoBehaviour
     private float moveAmount;
     void Start()
     {
+        Canvas.ForceUpdateCanvases();
         current.SetText("0");
         toUpdate.SetText("0");
         containerInitPosition = coinTextContainer.localPosition.y;
